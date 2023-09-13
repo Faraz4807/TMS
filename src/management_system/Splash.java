cd10b414a909f48769895bf78bfd831c8ecd85cf
Faraz4807 <farajm624@mail.com>
2023-09-13 12:22:01 +0530

    second commit

--- src/management_system/Splash.java
+++ src/management_system/Splash.java
@@ -10,7 +10,7 @@ public class Splash {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello world");
+		System.out.print("Hello Everyone good morning ");
 
 	}
 
